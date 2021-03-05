860eb6d6a818ca0c2a3183839fe6fdf2b7651869
Will <willunsworth16@gmail.com>
2021-03-05 11:31:06 +0000

    Added docstrings

--- airpollutionmodel/sensor.py
+++ airpollutionmodel/sensor.py
@@ -4,8 +4,22 @@ import numpy as np
 import pandas as pd
 
 class Sensor:
-    def __init__(self, ID, latitude, longitude):
+    """ A sensor class containing ID, location, readings etc.
+    
+    This class is a parent class for our three types of sensors: high-cost static, low-cost static, and low-cost mobile.
+    These objects are intended to form the class structure of part of our Network class.
+
+    Attributes:
+        ID: the unique ID of the sensor (str)
+        sensorType: the type of sensor (high, low, mobile) (str)
+        latitude: the latitude coordinate of the sensor (float)
+        longitude: the longitude coordinate of the sensor (float)
+        readings: the readings taken from the sensor indexed by datetime (dict)
+        calibrations: a record of when the sensor has been calibrated indexed by datetime (dict)
+    """
+    def __init__(self, ID, sensorType, latitude, longitude):
         self.ID = ID
+        self.sensorType = sensorType
         self.latitude = latitude
         self.longitude = longitude
         self.readings = dict()
@@ -31,27 +45,76 @@ class Sensor:
         return json.dumps(d, indent = 4)
 
 class mobileSensor(Sensor):
+    """ A subclass of the Sensor class describing mobile sensors
+
+    This sensor is low-cost, so it needs to be calibrated.
+    
+    Attributes:
+        Attributes inherited from Sensor class.
+        calib_time: the last time the sensor was calibrated (datetime)
+    """
     def __init__(self, ID, latitude, longitude, calib_time = None):
-        super().__init__(ID, latitude, longitude)
+        super().__init__(ID = ID, sensorType = 'mobile', latitude = latitude, longitude = longitude)
         self.calib_time = calib_time
         
 class lowSensor(Sensor):
+    """A subclass of the Sensor class describing the low-cost static sensors
+
+    This sensor is low-cost, so it needs to be calibrated.
+    
+    Attributes:
+        Attributes inherited from Sensor class.
+        calib_time: the last time the sensor was calibrated (datetime)
+    """
     def __init__(self, ID, latitude, longitude, calib_time = None):
-        super().__init__(ID, latitude, longitude)
+        super().__init__(ID = ID, sensorType = 'low', latitude = latitude, longitude = longitude)
         self.calib_time = calib_time
 
+class highSensor(Sensor):
+    """A subclass of the Sensor class describing the high-cost static sensors
+    
+    Attributes:
+        Attributes inherited from Sensor class.
+    """
+    def __init__(self, ID, latitude, longitude):
+        super().__init__(ID = ID, sensorType = 'high', latitude = latitude, longitude = longitude)
+
 class Network:
-    def __init__(self):
-        self.sensors = dict()
+    """ Describes a sensor network.
+
+    Attributes:
+        sensors: a list of Sensor objects, these can be of any type. (list(Sensor))
+        IDs: a list of the IDs of the Sensor objects, these can be useful for iterating over.
+    """
+    def __init__(self, sensors: list):
+        self.sensors = {sensor.ID: sensor for sensor in sensors}
+        self.IDs = [sensor.ID for sensor in sensors]
     
     def add_sensors(self, sensors):
+        """ Takes either a Sensor object or a list of Sensor objects and adds them to the network
+
+        Args:
+            sensors: a Sensor object or list of Sensor objects to be added. (Sensor/list(Sensor))
+
+        Returns:
+            None            
+        """
         sensors = list(sensors)
         for sensor in sensors:
-            self.sensor_list[sensor.ID] = sensor
+            self.sensors[sensor.ID] = sensor
+            self.IDs.append(sensor.ID)
 
-    def ids(self):
-        return [sensor.ID for sensor in self.sensors()]
-    
+    def networkData(self, df):
+        """ Takes a dataframe and returns all the data for the sensors in our network
+
+        Args:
+            self: the Network object containing our Sensors
+            df: the pandas dataframe containing all the data
+
+        Returns:
+            networkDF: the pandas dataframe containing data for our sensors
+        """
+
     def readings(self, since: datetime):
         readings = dict()
         for sensor in self.sensors:
